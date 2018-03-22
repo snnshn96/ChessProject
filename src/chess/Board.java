d00aa2103adb0baf57af0787efea65214d1ef2fa
Sinan Sahin <sinan.sahin@rutgers.edu>
2018-03-22 19:05:24 -0400

    Implemented more efficient checking for checkmate

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -111,78 +111,46 @@ public class Board {
 			for (int col = 0; col < 8; col++) {
 				Piece currpiece = board.pieces[row][col];
 				if (currpiece != null && currpiece instanceof Piece && currpiece.black != isBlack) {
-					// ==========   King   ==========
+					int[][] offsets = {};
+					int newRow = 0;
+					int newCol = 0;
+					// ========== King ==========
 					if (currpiece instanceof King) {
-						int[][] offsets = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 }, { 1, 1 }, { -1, 1 }, { -1, -1 }, { 1, -1 } };
-						int newRow = 0;
-						int newCol = 0;
-						
-						for (int[] off : offsets) {
-							newRow = off[0] + row;
-							newCol = off[1] + col;
-							if(newRow <=7 && newRow >=0 && newCol <=7 && newCol >=0) {
-								int[] moves = new int[] { col, row, newCol, newRow };
-								tempBoard = tempBoard.pieces[moves[1]][moves[0]].makemove(tempBoard, moves);
-								if (!Arrays.deepEquals(tempBoard.pieces, board.pieces) && !isItCheck(tempBoard, !isBlack)) {
-									return false;
-								} else {
-									tempBoard.pieces = Chess.copy(board.pieces);
-								}
-							}
-						}
+						offsets = new int[][] { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 }, { 1, 1 }, { -1, 1 },
+								{ -1, -1 }, { 1, -1 } };
 					}
-					// ==========   Pawn   ==========
+					// ========== Pawn ==========
 					if (currpiece instanceof Pawn) {
 						int[][] blackPawnOffs = { { -1, 0 }, { -2, 0 }, { -1, -1 }, { -1, 1 } };
 						int[][] whitePawnOffs = { { 1, 0 }, { 2, 0 }, { 1, -1 }, { 1, 1 } };
-						int newRow = 0;
-						int newCol = 0;
-						int[][] useOff = blackPawnOffs;
+						offsets = blackPawnOffs;
 						if (isBlack)
-							useOff = whitePawnOffs;
-
-						for (int[] off : useOff) {
-							newRow = off[0] + row;
-							newCol = off[1] + col;
-							if (newRow <= 7 && newRow >= 0 && newCol <= 7 && newCol >= 0) {
-								int[] moves = new int[] { col, row, newCol, newRow };
-								tempBoard = tempBoard.pieces[moves[1]][moves[0]].makemove(tempBoard, moves);
-								if (!Arrays.deepEquals(tempBoard.pieces, board.pieces) && !isItCheck(tempBoard, !isBlack)) {
-									return false;
-								} else {
-									tempBoard.pieces = Chess.copy(board.pieces);
-								}
-							}
-						}
+							offsets = whitePawnOffs;
 					}
-					
-					// ==========   Rook or Queen Hor&Ver   ==========
-					if (currpiece instanceof Rook || currpiece instanceof Queen) {
-						
-					}
-					// ==========   Bishop or Queen Diag   ==========
-					if (currpiece instanceof Bishop || currpiece instanceof Queen) {
-						
-					}
-					// ==========   Knight   ==========
+					// ========== Knight ==========
 					if (currpiece instanceof Knight) {
-						if (currpiece instanceof King) {
-							int[][] offsets = { { 1, 2 }, { 2, 1 }, { -1, 2 }, { -2, 1 }, { 1, -2 }, { 2, -1 }, { -1, -2 }, { -2, -1 } };
-							int newRow = 0;
-							int newCol = 0;
-							
-							for (int[] off : offsets) {
-								newRow = off[0] + row;
-								newCol = off[1] + col;
-								if(newRow <=7 && newRow >=0 && newCol <=7 && newCol >=0) {
-									int[] moves = new int[] { col, row, newCol, newRow };
-									tempBoard = tempBoard.pieces[moves[1]][moves[0]].makemove(tempBoard, moves);
-									if (!Arrays.deepEquals(tempBoard.pieces, board.pieces) && !isItCheck(tempBoard, !isBlack)) {
-										return false;
-									} else {
-										tempBoard.pieces = Chess.copy(board.pieces);
-									}
-								}
+						offsets = new int[][] { { 1, 2 }, { 2, 1 }, { -1, 2 }, { -2, 1 }, { 1, -2 }, { 2, -1 },
+								{ -1, -2 }, { -2, -1 } };
+					}
+					// ========== Rook or Queen Hor&Ver ==========
+					if (currpiece instanceof Rook || currpiece instanceof Queen) {
+
+					}
+					// ========== Bishop or Queen Diag ==========
+					if (currpiece instanceof Bishop || currpiece instanceof Queen) {
+
+					}
+					// ========== Check Moves ========== //
+					for (int[] off : offsets) {
+						newRow = off[0] + row;
+						newCol = off[1] + col;
+						if (newRow <= 7 && newRow >= 0 && newCol <= 7 && newCol >= 0) {
+							int[] moves = new int[] { col, row, newCol, newRow };
+							tempBoard = tempBoard.pieces[moves[1]][moves[0]].makemove(tempBoard, moves);
+							if (!Arrays.deepEquals(tempBoard.pieces, board.pieces) && !isItCheck(tempBoard, !isBlack)) {
+								return false;
+							} else {
+								tempBoard.pieces = Chess.copy(board.pieces);
 							}
 						}
 					}
